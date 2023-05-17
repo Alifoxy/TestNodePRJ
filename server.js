6068113fd2599decaa1c2587f934d717e6224ca5
Alifoxy <nightfoxy666@gmail.com>
2023-05-17 15:23:50 +0300

    test1

--- server.js
+++ server.js
@@ -2,3 +2,16 @@ const express = require("express")
 const app = express()
 const PORT = 5000
 app.listen(PORT, () => console.log(`Server Connected to port ${PORT}`))
+
+const connectDB = require("./db");
+connectDB();
+
+const server = app.listen(PORT, () =>
+    console.log(`Server Connected to port ${PORT}`)
+)
+
+process.on("unhandledRejection", err => {
+    console.log(`An error occurred: ${err.message}`)
+    server.close(() => process.exit(1))
+})
+
